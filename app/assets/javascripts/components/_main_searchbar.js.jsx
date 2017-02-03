d732e1161ab351b7176d6f0754e530b2621d9e05
antciccone <aciccone@bu.edu>
2017-02-02 19:54:23 -0700

    search bar in place

--- app/assets/javascripts/components/_main_searchbar.js.jsx
+++ app/assets/javascripts/components/_main_searchbar.js.jsx
@@ -1,10 +1,14 @@
 var MainSearch = React.createClass({
   render: function() {
     return (
-      <div className="region-filters">
-        <form><input type="text" placeholder="Search here..." name="firstname" />
-       </form>
-      </div>
+        <div className="region-filters">
+        <div className="search-container">
+         <form action className="Search">
+           <input className="Search-box" placeholder="Search Here..." type="search" id="Search-box" autoComplete="off" />
+           <label className="Search-label" htmlFor="Search-box"><i className="material-icons">search</i></label>
+         </form>
+         </div>
+       </div>
     )
   }
 })
