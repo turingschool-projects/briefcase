91af5df67885b473d4a5960c6cf23b0887c665b4
antciccone <aciccone@bu.edu>
2017-02-04 14:02:56 -0700

    add card to account page

--- app/assets/javascripts/components/_account.js.jsx
+++ app/assets/javascripts/components/_account.js.jsx
@@ -6,46 +6,25 @@ var Account = React.createClass({
         <DashboardJumbo/>
         <div className="container">
           <div className="row">
+            <div className="col s6"><input></input><lable>Current Password</lable></div>
+              <div className="col s6"><input></input><lable>Name</lable></div>
+          </div>
+          <div className="row">
+            <div className="col s6"><input></input><lable>Email Address</lable></div>
             <div className="col s6">
-              <input>
-
-              </input>
-              <label>
-                Current password
-              </label>
-              <input>
-
-              </input>
-              <label>
-                Email Address
-              </label>
-              <div className="row">
-                <div className='col s6'>
-                  <input>
-
-                  </input>
-                  <label>
-                    Password
-                  </label>
-                </div>
-                <div className='col s6'>
-                  <input>
-
-                  </input>
-                  <label>
-                    Confirm Password
-                  </label>
-
-                </div>
-                <div className='col s6'>
-
-
-                </div>
+              <div class="card-panel teal">
+                <span class="white-text">I am a very simple card. I am good at containing small bits of information.
+                  I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+                </span>
+              </div>
             </div>
-            <div className="col s6">
-              <a className="waves-effect waves-light btn">button</a>
-            </div>
-            </div>
+          </div>
+          <div className="row">
+            <div className="col s3"><input></input><lable>Passord</lable></div>
+            <div className="col s3"><input></input><lable>Confirm Password</lable></div>
+          </div>
+          <div className="row">
+            <a className="waves-effect waves-light btn account-btn">Save</a>
           </div>
         </div>
         <Footer/>
