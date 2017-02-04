ce1404fca9678ab97a4343f9c5f4481611dc3a6c
antciccone <aciccone@bu.edu>
2017-02-04 13:46:44 -0700

    add form to account edit page

--- app/assets/javascripts/components/_account.js.jsx
+++ app/assets/javascripts/components/_account.js.jsx
@@ -4,6 +4,51 @@ var Account = React.createClass({
       <div>
         <SignedInNavbar/>
         <DashboardJumbo/>
+        <div className="container">
+          <div className="row">
+            <div className="col s6">
+              <input>
+
+              </input>
+              <label>
+                Current password
+              </label>
+              <input>
+
+              </input>
+              <label>
+                Email Address
+              </label>
+              <div className="row">
+                <div className='col s6'>
+                  <input>
+
+                  </input>
+                  <label>
+                    Password
+                  </label>
+                </div>
+                <div className='col s6'>
+                  <input>
+
+                  </input>
+                  <label>
+                    Confirm Password
+                  </label>
+
+                </div>
+                <div className='col s6'>
+
+
+                </div>
+            </div>
+            <div className="col s6">
+              <a className="waves-effect waves-light btn">button</a>
+            </div>
+            </div>
+          </div>
+        </div>
+        <Footer/>
       </div>
     );
   }
