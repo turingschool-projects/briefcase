abaa41319be8c0e87e554b8d68bce06dc959bf10
antciccone <aciccone@bu.edu>
2017-02-04 14:24:17 -0700

    account page work in progress

--- app/assets/javascripts/components/_account.js.jsx
+++ app/assets/javascripts/components/_account.js.jsx
@@ -5,18 +5,15 @@ var Account = React.createClass({
         <SignedInNavbar/>
         <DashboardJumbo/>
         <div className="container">
-          <div className="row">
+          <div className="row account-row">
             <div className="col s6"><input></input><lable>Current Password</lable></div>
               <div className="col s6"><input></input><lable>Name</lable></div>
           </div>
           <div className="row">
             <div className="col s6"><input></input><lable>Email Address</lable></div>
             <div className="col s6">
-              <div class="card-panel teal">
-                <span class="white-text">I am a very simple card. I am good at containing small bits of information.
-                  I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
-                </span>
-              </div>
+
+
             </div>
           </div>
           <div className="row">
