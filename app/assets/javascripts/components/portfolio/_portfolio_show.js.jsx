590e267cfa89dffd64e6453b612a5c045910d8d4
Daniel Shin <shin.daniel.d@gmail.com>
2017-02-06 23:21:42 -0700

    link projects to portfolio show

--- app/assets/javascripts/components/portfolio/_portfolio_show.js.jsx
+++ app/assets/javascripts/components/portfolio/_portfolio_show.js.jsx
@@ -1,13 +1,15 @@
 var PortfolioShow = React.createClass({
   render: function(){
       var user = this.props.user;
+      var projects = this.props.projects;
+      
       return(
         <div>
           <SignedInNavbar/>
           <PortfolioJumbo user = {user}/>
           <PortfolioJumboInfo user = {user}/>
           <PortfolioBodyInfo user = {user}/>
-          
+          <UserProjectCard user = {user} projects={projects}/>
           <Footer/>
         </div>
       )
