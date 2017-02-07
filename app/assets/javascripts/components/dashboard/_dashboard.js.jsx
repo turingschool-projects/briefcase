83258627663e11e62c50d0a72d6a6d7460fd47d0
Daniel Shin <shin.daniel.d@gmail.com>
2017-02-06 19:42:30 -0700

    ternary card dispay

--- app/assets/javascripts/components/dashboard/_dashboard.js.jsx
+++ app/assets/javascripts/components/dashboard/_dashboard.js.jsx
@@ -1,38 +1,21 @@
 var Dashboard = React.createClass({
   render(){
     var user = this.props.user;
-    var projects = [1];
+    var projects = this.props.projects;
     var portfolio = this.props.portfolio;
 
-    if(portfolio != null) {
-      return (
-        <div>
-          <SignedInNavbar/>
-          <DashboardJumbo/>
-            <div className="container dashboard-padding">
-              <div className="row">
-                <DashboardProfile user={user}/>
-                <DashboardProject user={user} projects={projects}/>
-              </div>
+    return (
+      <div>
+        <SignedInNavbar/>
+        <DashboardJumbo/>
+          <div className="container dashboard-padding">
+            <div className="row">
+              { portfolio != null ? <DashboardProfile user={user}/> : <DashboardDefaultCard user={user}/>}
+              <DashboardProject user={user} projects={projects}/>
             </div>
-            <Footer/>
-        </div>
-      )
-    } else {
-      return (
-        <div>
-          <SignedInNavbar/>
-          <DashboardJumbo/>
-            <div className="container dashboard-padding">
-              <div className="row">
-                <DashboardDefaultCard user={user}/>
-                <DashboardProject user={user} projects={projects}/>
-              </div>
-            </div>
-            <Footer/>
-        </div>
-      )
-    }
-
+          </div>
+          <Footer/>
+      </div>
+    )
   }
 })
