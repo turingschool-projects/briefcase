fdb6de9fb6d83cb2b400bc5f2b677f60231e87ba
antciccone <aciccone@bu.edu>
2017-02-07 15:40:38 -0700

    fix bio issue

--- app/assets/javascripts/components/dashboard/_dashboard.js.jsx
+++ app/assets/javascripts/components/dashboard/_dashboard.js.jsx
@@ -3,14 +3,13 @@ var Dashboard = React.createClass({
     var user = this.props.user;
     var projects = this.props.projects;
     var portfolio = this.props.portfolio;
-
     return (
       <div>
         <SignedInNavbar/>
         <DashboardJumbo/>
           <div className="container dashboard-padding">
             <div className="row">
-              { portfolio != null ? <DashboardProfile user={user}/> : <DashboardDefaultCard user={user}/>}
+              { portfolio != null ? <DashboardProfile user={user} portfolio={portfolio}/> : <DashboardDefaultCard user={user} portfolio={portfolio}/>}
               <DashboardProject user={user} projects={projects}/>
             </div>
           </div>
