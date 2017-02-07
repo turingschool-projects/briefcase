35ba54f45c0c0b0ecb838221b6765d3361933b55
Matt D <MDes41@users.noreply.github.com>
2017-02-06 18:09:13 -0700

    Add alumni path to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,11 @@ Rails.application.routes.draw do
 
   get  'auth/:provider/callback',    to: 'sessions#create'
   get  'sign_in_with_census',    to: 'sessions#new'
-  root 'users#index'
+  get '/', to: redirect('/alumni')
   get '/logout', to: 'sessions#destroy'
 
   get '/dashboard', to: 'dashboard#index'
-  resources :users, only: [:index, :edit, :update, :show]
+  resources :users, only: [:index, :edit, :update, :show], param: :slug, path: 'alumni'
   get '/user/1/edit-account', to: 'users#account'
 
-  namespace :admin do
-    resources :users, only: [:index, :create, :destroy]
-  end
 end
