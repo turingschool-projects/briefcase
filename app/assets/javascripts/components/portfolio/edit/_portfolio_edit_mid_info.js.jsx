c00ff918fd11df6b11e5cab279ba3a220a1f3152
Daniel Shin <shin.daniel.d@gmail.com>
2017-02-08 01:52:43 -0700

    materialize locations dropdown

--- app/assets/javascripts/components/portfolio/edit/_portfolio_edit_mid_info.js.jsx
+++ app/assets/javascripts/components/portfolio/edit/_portfolio_edit_mid_info.js.jsx
@@ -1,6 +1,6 @@
 var PortfolioEditMidInfo = React.createClass({
-  handleChange(){
-
+  componentDidMount(){
+    $('select').material_select();
   },
 
   render: function() {
@@ -15,11 +15,11 @@ var PortfolioEditMidInfo = React.createClass({
             <h1 id="edit-profile-information">Profile Information</h1>
             <div className="row">
               <div className='col s5'>
-                <label for="email">Email</label>
+                <label htmlFor="email">Email</label>
                 <input id="email" placeholder="example@example.com"></input>
               </div>
               <div className='col s5 offset-s2'>
-                <label for="resume">Email</label><br/>
+                <label htmlFor="resume">Email</label><br/>
                 <input id="resume" type="file"></input>
               </div>
             </div>
@@ -28,27 +28,25 @@ var PortfolioEditMidInfo = React.createClass({
           <div className="col s12">
             <div className="row mid-bio">
               <div className='col s6'>
-                <label for="looking-for">Looking For</label>
+                <label htmlFor="looking-for">Looking For</label>
                 <textarea id="looking-for" placeholder="example@example.com"></textarea>
               </div>
               <div className='col s6'>
-                <label for="best-at">Best At</label>
+                <label htmlFor="best-at">Best At</label>
                 <textarea id="best-at" type="file"></textarea>
               </div>
             </div>
           </div>
 
-          <div className="col s12">
-            <h2>Preferred Locations</h2>
-            <select>
-              <option value="grapefruit">Grapefruit</option>
-              <option value="lime">Lime</option>
-              <option selected value="coconut">Coconut</option>
-              <option value="mango">Mango</option>
+          <div class="input-field col s12">
+            <select multiple>
+                <option value="" disabled selected>Preferred Locations</option>
+                <option value="1">Option 1</option>
+                <option value="2">Option 2</option>
+                <option value="3">Option 3</option>
             </select>
-          </div>
-
-
+            <label>Materialize Multiple Select</label>
+        </div>
 
         </div>
       </div>
