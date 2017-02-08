789608aa3f09c690c8b58cbf564cb343f4cbaf5a
Matt D <MDes41@users.noreply.github.com>
2017-02-07 21:03:34 -0700

    Cleaned up methods on delete

--- app/assets/javascripts/components/portfolio/delete/_delete_portfolio_body.js.jsx
+++ app/assets/javascripts/components/portfolio/delete/_delete_portfolio_body.js.jsx
@@ -1,11 +1,6 @@
 
 var DeletePortfolioBody = React.createClass({
 
-  getInitialState(){
-    var userPortfolio = this.props.portfolio;
-    return { portfolio: userPortfolio }
-  },
-
   handleDelete(){
     var user = this.props.user;
 
@@ -28,7 +23,7 @@ var DeletePortfolioBody = React.createClass({
               <p>This action cannot be undone.</p>
             </div>
             <div className="col s12 delete-button-row">
-              <a className="waves-effect waves-light btn delete" onClick={this.handleDelete}><i className="fa fa-trash-o" aria-hidden="true"></i> Delete </a>
+              <a className="waves-effect waves-light btn red delete" onClick={this.handleDelete}><i className="fa fa-trash-o" aria-hidden="true"></i> Delete </a>
               <a className="cancel" href='/dashboard' >Cancel</a>
             </div>
           </div>
