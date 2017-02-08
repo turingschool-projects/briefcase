bd6ba3fd412477995b46cdaf2abb2c360d77945f
antciccone <aciccone@bu.edu>
2017-02-08 14:41:48 -0700

    add form input for project name, github and production url

--- app/assets/javascripts/components/project/_project_form.js.jsx
+++ app/assets/javascripts/components/project/_project_form.js.jsx
@@ -1,38 +1,23 @@
 var ProjectForm = React.createClass({
   render: function(){
     return(
-      <div className="container">
-      <div className="row project-form">
+      <div className="container project-form">
+      <div className="row project-row">
           <form className="col s12">
             <div className="row">
               <div className="input-field col s6">
-                <input placeholder="Project Name" id="first_name" type="text" className="validate" />
+                <input placeholder="Project Name*" id="first_name" type="text" className="validate" />
               </div>
               <div className="input-field col s6">
-                <input placeholder="Github Url" id="last_name" type="text" className="validate" />
+                <input placeholder="Github Url*" id="last_name" type="text" className="validate" />
               </div>
             </div>
             <div className="row">
-              <div className="input-field col s12">
-                <input disabled defaultValue="I am not editable" id="disabled" type="text" className="validate" />
+              <div className="input-field col s6">
+                <input placeholder="Production Url" id="email" type="email" className="validate" />
               </div>
-            </div>
-            <div className="row">
-              <div className="input-field col s12">
-                <input id="password" type="password" className="validate" />
-              </div>
-            </div>
-            <div className="row">
-              <div className="input-field col s12">
-                <input id="email" type="email" className="validate" />
-              </div>
-            </div>
-            <div className="row">
-              <div className="col s12">
-                This is an inline input field:
-                <div className="input-field inline">
-                  <input id="email" type="email" className="validate" />
-                </div>
+              <div className="input-field col s6">
+                <input placeholder="Upload Screen Shot" id="email" type="email" className="validate" />
               </div>
             </div>
           </form>
