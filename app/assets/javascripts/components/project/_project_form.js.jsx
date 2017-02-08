aa3c04e34dce0cb98d7df0b231e36a14afa3f229
antciccone <aciccone@bu.edu>
2017-02-08 15:07:51 -0700

    add text areas for filling in info about your project

--- app/assets/javascripts/components/project/_project_form.js.jsx
+++ app/assets/javascripts/components/project/_project_form.js.jsx
@@ -19,9 +19,9 @@ var ProjectForm = React.createClass({
               <div className="input-field col s6 ">
                 <form action="#">
                  <div className="file-field input-field">
-                   <div className="btn">
+                   <div className="btn file">
                      <span>File</span>
-                     <input type="file"/>
+                     <input  type="file"/>
                    </div>
                    <div className="file-path-wrapper">
                      <input placeholder="Upload Screen Shot" className="file-path validate" type="text" />
@@ -31,6 +31,18 @@ var ProjectForm = React.createClass({
               </div>
             </div>
           </form>
+          <section className="col s12">
+            <div className="row mid-bio">
+              <div className='col s6'>
+                <label htmlFor="looking-for">Description of project</label>
+                <textarea id="looking-for" placeholder="example@example.com"></textarea>
+              </div>
+              <div className='col s6'>
+                <label htmlFor="best-at">My Areas of Focus</label>
+                <textarea id="best-at" type="file"></textarea>
+              </div>
+            </div>
+          </section>
         </div>
         <a className="waves-effect waves-light btn project-save" data-reactid="54"><i className="fa fa-check" aria-hidden="true"></i>Save Project </a>
       </div>
