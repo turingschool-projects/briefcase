46ba19b8f71f98de518dba3537a048174b1b3f85
antciccone <aciccone@bu.edu>
2017-02-16 16:47:13 -0700

    changed dummy navbar

--- app/assets/javascripts/components/shared/_guest_navbar.js.jsx
+++ app/assets/javascripts/components/shared/_guest_navbar.js.jsx
@@ -18,14 +18,14 @@ var GuestNavbar = React.createClass({
       </div>
       <div role="navigation" className="pull-right visible-lg menu-container">
         <ul className="menu nav navbar-nav">
-          <li className=""><a href="/"><span>The Difference</span></a></li>
-          <li className=""><a href="/users/1/edit"><span>Our Team</span></a></li>
-          <li className=""><a href="#"><span>Programs</span></a></li>
-          <li className=""><a href="#"><span>Admissions</span></a></li>
-          <li className=""><a href="#"><span>Tuition</span></a></li>
-          <li className=""><a href="#"><span>Blog</span></a></li>
-          <li className=""><a href="#"><span>Contact</span></a></li>
-          <li className=""><a href="#"><span>Hire a Grad</span></a></li>
+          <li className="https://www.turing.io/turing-difference"><a href="/"><span>The Difference</span></a></li>
+          <li className="https://www.turing.io/our-team"><a href="/users/1/edit"><span>Our Team</span></a></li>
+          <li className="https://www.turing.io/programs"><a href="#"><span>Programs</span></a></li>
+          <li className="https://www.turing.io/admissions"><a href="#"><span>Admissions</span></a></li>
+          <li className="https://www.turing.io/tuition"><a href="#"><span>Tuition</span></a></li>
+          <li className="https://www.turing.io/blog"><a href="#"><span>Blog</span></a></li>
+          <li className="https://www.turing.io/contact"><a href="#"><span>Contact</span></a></li>
+          <li className="https://www.turing.io/why-hire-grad"><a href="#"><span>Hire a Grad</span></a></li>
           <li className=""><a href="/auth/census"><span>Log In</span></a></li>
         </ul>
       </div>
