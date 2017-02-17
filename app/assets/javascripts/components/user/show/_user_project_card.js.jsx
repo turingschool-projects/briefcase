667b5711b69cb3c17c3c6c88beef6da189312972
antciccone <aciccone@bu.edu>
2017-02-16 17:30:44 -0700

    change to project urls

--- app/assets/javascripts/components/user/show/_user_project_card.js.jsx
+++ app/assets/javascripts/components/user/show/_user_project_card.js.jsx
@@ -3,6 +3,7 @@ var UserProjectCard = React.createClass ({
 		var all_projects = this.props.projects.map((project) => {
 			// var production_url = project.production_url.split('//')
 			// var github = project.github.split('//')
+			debugger;
 			return (
 						<div className='projects'>
 							<div className='title col s12'>
@@ -22,9 +23,9 @@ var UserProjectCard = React.createClass ({
 							</div>
 							<div className='col s12 project-buttons'>
 	              <ul>
-	                <li><button className='btn transparent repository-btn'><a href={ 'http://' + github[github.length - 1]}>Repository Page <i className="fa fa-long-arrow-right" aria-hidden="true"></i></a> </button></li>
-	                <li><button className='btn transparent launch-app-btn'><a href={ 'http://' + production_url[production_url.length - 1] }>Launch Application <i className="fa fa-long-arrow-right" aria-hidden="true"></i></a> </button></li>
-	              </ul>
+	                <li><button className='btn transparent repository-btn'><a href={ 'http://' + project.github}>Repository Page <i className="fa fa-long-arrow-right" aria-hidden="true"></i></a> </button></li>
+									<li><button className='btn transparent launch-app-btn'><a href={ 'http://' + project.production_url }>Launch Application <i className="fa fa-long-arrow-right" aria-hidden="true"></i></a> </button></li>
+							</ul>
               </div>
               <div className='row'></div>
 						</div>
