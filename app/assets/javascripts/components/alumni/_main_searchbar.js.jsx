6fff5a98c730b35066151ca63d4f95afeca4c340
antciccone <aciccone@bu.edu>
2017-02-20 10:30:25 -0700

    user and filter alumni by hearts

--- app/assets/javascripts/components/alumni/_main_searchbar.js.jsx
+++ app/assets/javascripts/components/alumni/_main_searchbar.js.jsx
@@ -1,7 +1,13 @@
 var MainSearch = React.createClass({
 
-  showHearts: function(event){
-    $('.fa-heart')[0].parentElement
+  showHearts: function(self){
+    $(self.target).toggleClass("fa-heart-o fa-heart");
+    if (self.target.className === "fa left fa-2x fa-heart") {
+      $('.card').hide()
+      $('.card .fa-heart').parent().show()
+    } else {
+      $('.card').show()
+    }
   },
 
   render: function() {
@@ -11,8 +17,8 @@ var MainSearch = React.createClass({
          <form action className="Search">
            <input className="Search-box" placeholder="Search Here..." type="search" id="Search-box" autoComplete="off" />
            <label className="Search-label" htmlFor="Search-box"><i className="material-icons">search</i></label>
-              {/* <button className="btn" onClick={this.showHearts}>by hearts</button> */}
          </form>
+         <i className="fa fa-heart-o left fa-2x" aria-hidden="true" onClick={this.showHearts}></i>
          </div>
        </div>
     )
