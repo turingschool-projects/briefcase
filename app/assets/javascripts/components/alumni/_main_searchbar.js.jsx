937f91e51793cfab7436bec9daae970e26aef9cb
antciccone <aciccone@bu.edu>
2017-02-20 10:46:01 -0700

    css for heart

--- app/assets/javascripts/components/alumni/_main_searchbar.js.jsx
+++ app/assets/javascripts/components/alumni/_main_searchbar.js.jsx
@@ -14,11 +14,11 @@ var MainSearch = React.createClass({
     return (
         <div className="region-filters">
         <div className="search-container">
+          <i id="by-heart" className="fa fa-heart-o left fa-2x" aria-hidden="true" onClick={this.showHearts}></i>
          <form action className="Search">
            <input className="Search-box" placeholder="Search Here..." type="search" id="Search-box" autoComplete="off" />
            <label className="Search-label" htmlFor="Search-box"><i className="material-icons">search</i></label>
          </form>
-         <i className="fa fa-heart-o left fa-2x" aria-hidden="true" onClick={this.showHearts}></i>
          </div>
        </div>
     )
