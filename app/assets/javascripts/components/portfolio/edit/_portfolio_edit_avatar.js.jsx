36287a990d572a30b7516be62061fb493627328e
antciccone <aciccone@bu.edu>
2017-02-22 14:00:21 -0700

    fixed css for avatar button

--- app/assets/javascripts/components/portfolio/edit/_portfolio_edit_avatar.js.jsx
+++ app/assets/javascripts/components/portfolio/edit/_portfolio_edit_avatar.js.jsx
@@ -38,9 +38,9 @@ render() {
       $imagePreview = (<img src={imagePreviewUrl} />);
     } else {
       $imagePreview = (
-                          this.props.avatar === "/avatars/original/missing.png" 
+                          this.props.avatar === "/avatars/original/missing.png"
                             ? <img className="" src="http://intelligentsystemsmonitoring.com/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png"></img>
-                            : <img className="" src={this.props.avatar}></img> 
+                            : <img className="" src={this.props.avatar}></img>
                       );
     }
 
@@ -50,7 +50,7 @@ render() {
         <div className="img-preview">
           {$imagePreview}
         </div>
-        <div className='button-input inner'><input id='file-input-avatar' className="file file-input" type="file" onChange={this.handleImageChange} /></div>
+        <div className='button-input inner'><input id='file-input-avatar' className="file-input" type="file" onChange={this.handleImageChange} /></div>
       </div>
     )
   }
