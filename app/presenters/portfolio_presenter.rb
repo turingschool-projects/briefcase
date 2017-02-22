509564b099ec2cab743b16926bb824c1984ec475
Matt D <MDes41@users.noreply.github.com>
2017-02-21 21:21:20 -0700

    Added props to presenter

--- app/presenters/portfolio_presenter.rb
+++ app/presenters/portfolio_presenter.rb
@@ -18,4 +18,12 @@ class PortfolioPresenter
   def locations
     @portfolio.locations
   end
+
+  def desc
+    @portfolio.order(full_name: :desc)
+  end
+
+  def asc
+    @portfolio.order(full_name: :asc)
+  end
 end
