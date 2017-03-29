d7f64883c601f11d36aa7d3a1a28b7ee1d2cd166
Daniel Rodriguez <drod2793@gmail.com>
2017-03-28 21:06:45 -0600

    Removes logic from dashboard index action

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,11 +2,6 @@ class DashboardController < ApplicationController
   def index
     if (logged_in?)
       @dashboard_presenter = DashboardPresenter.new(current_user)
-      if(current_user.portfolio)
-        @projects = @dashboard_presenter.projects
-      else
-        @projects = []
-      end
     else
       redirect_to root_path
     end
