34749656f2ee070b3e277d87d98d35c77aeb43b1
Daniel Rodriguez <drod2793@gmail.com>
2017-03-28 20:41:58 -0600

    Rearranges logic to fix failing tests

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,9 @@
 class DashboardController < ApplicationController
   def index
     if (logged_in?)
+      @dashboard_presenter = DashboardPresenter.new(current_user)
       if(current_user.portfolio)
-        @dashboard_presenter = DashboardPresenter.new(current_user)
+        @projects = @dashboard_presenter.projects
       else
         @projects = []
       end
