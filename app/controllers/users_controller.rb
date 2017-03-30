0311eb036ab0a2e8114fd98b1e972ff3e8743db1
Daniel Rodriguez <drod2793@gmail.com>
2017-03-29 20:57:09 -0600

    Removes unused index and show actions from users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,4 @@
 class UsersController < ApplicationController
-  def index
-
-    @users = User.all
-    if(logged_in?)
-      render component: 'Main', props: { users: @users, signed_in: 1 }
-    else
-      render component: 'Main', props: { users: @users, signed_in: 0 }
-    end
-  end
-
-  def show
-    @user = User.find_by(slug: params["slug"])
-    render component: "Show", props: { user: @user, projects: @user.projects }
-  end
-
   def edit
     user = User.find_by(slug: params["user_slug"])
     render component: "Edit", props: { user: @user }
