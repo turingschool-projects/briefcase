b22d9e966728a88426b4e8743018552b45d6a261
Laszlo Balogh <laszlo@thebaloghs.com>
2017-03-30 12:13:34 -0600

    Refactor PortfolioForReact into a separate model file and simplify the way it is called with single argument of portfolio object from database

--- app/presenters/portfolio_presenter.rb
+++ app/presenters/portfolio_presenter.rb
@@ -4,9 +4,7 @@ class PortfolioPresenter
   end
 
   def all
-    # @portfolios.all
-    output = build_up_portfolios
-    return output
+    build_up_portfolios
   end
 
   def avatar_urls
@@ -31,49 +29,39 @@ class PortfolioPresenter
 
   def build_up_portfolios
     @portfolios.all.map do |one_portfolio|
-      avatar_urls = {}
-      avatar_urls[:thumb] = one_portfolio.avatar.url(:thumb)
-      avatar_urls[:square] = one_portfolio.avatar.url(:square)
-      avatar_urls[:medium] = one_portfolio.avatar.url(:medium)
-      avatar_urls[:regular] = one_portfolio.avatar.url(:regular)
-      PortfolioForReact.new(one_portfolio.id,
-                            one_portfolio.title,
-                            one_portfolio.full_name,
-                            avatar_urls,
-                            one_portfolio.bio,
-                            one_portfolio.locations,
-                            one_portfolio.github_url,
-                            one_portfolio.linkedin_url,
-                            one_portfolio.twitter_url,
-                            one_portfolio.personal_url,
-                            one_portfolio.user_slug).output_for_react
+      PortfolioForReact.new(one_portfolio).output_for_react
     end
   end
 end
 
-class PortfolioForReact
-  attr_reader :portfolio
-  def initialize(id, title, full_name, avatar_urls, bio, locations, github_url, linkedin_url, twitter_url, personal_url, user_slug)
-    @portfolio = {
-      :id => id,
-      :title => title,
-      :full_name => full_name,
-      :avatar_urls => avatar_urls,
-      :bio => bio,
-      :locations => locations,
-      :github_url => github_url,
-      :linkedin_url => linkedin_url,
-      :twitter_url => twitter_url,
-      :personal_url => personal_url,
-      :user_slug => user_slug
-    }
-  end
+# class PortfolioForReact
+#   # def initialize(id, title, full_name, avatar_urls, bio, locations, github_url, linkedin_url, twitter_url, personal_url, user_slug)
+#   def initialize(portfolio)
+#     @portfolio = {
+#       :id => portfolio.id,
+#       :title => portfolio.title,
+#       :full_name => portfolio.full_name,
+#       :bio => portfolio.bio,
+#       :locations => portfolio.locations,
+#       :github_url => portfolio.github_url,
+#       :linkedin_url => portfolio.linkedin_url,
+#       :twitter_url => portfolio.twitter_url,
+#       :personal_url => portfolio.personal_url,
+#       :user_slug => portfolio.user_slug
+#     }
+#     avatar_urls = {}
+#     avatar_urls[:thumb] = portfolio.avatar.url(:thumb)
+#     avatar_urls[:square] = portfolio.avatar.url(:square)
+#     avatar_urls[:medium] = portfolio.avatar.url(:medium)
+#     avatar_urls[:regular] = portfolio.avatar.url(:regular)
+#     @portfolio[:avatar_urls] = avatar_urls
+#   end
 
-  def output_for_react
-    portfolio
-  end
+#   def output_for_react
+#     @portfolio
+#   end
 
-end
+# end
 
 # Copied here for reference to see what is needed within the other components
 # <%= react_component('Main', {portfolios: @portfolio.all,
