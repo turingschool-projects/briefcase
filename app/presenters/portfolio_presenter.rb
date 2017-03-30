0922e02e063e41d3345d57cc6f7d71c9398b25db
Laszlo Balogh <laszlo@thebaloghs.com>
2017-03-30 04:42:09 -0600

    Add initial skeleton of new portfolio PORO into portfolio_Presenter. Rename portfolio in origin portfolio_presenter to better reflect what it holds

--- app/presenters/portfolio_presenter.rb
+++ app/presenters/portfolio_presenter.rb
@@ -1,46 +1,64 @@
 class PortfolioPresenter
-  def initialize(portfolio)
-    @portfolio = portfolio
+  def initialize(portfolios)
+    @portfolios = portfolios
   end
 
   def all
-    @portfolio.all
+    @portfolios.all
   end
 
   def avatar_urls
-    @portfolio.avatar_urls
+    @portfolios.avatar_urls
   end
 
   def bios
-    @portfolio.markdown_bios
+    @portfolios.markdown_bios
   end
 
   def locations
-    @portfolio.locations
+    @portfolios.locations
   end
 
   def desc
-    @portfolio.order(full_name: :desc)
+    @portfolios.order(full_name: :desc)
   end
 
   def asc
-    @portfolio.order(full_name: :asc)
+    # @portfolio.order(full_name: :asc)
+    # Hijacked this method to test passing new PORO to Front-End
+    pf = Portfolio.first
+    PortfolioForReact.new()
   end
-
-  # def asc
-  #   TestPortfolio.new(@portfolio.first.full_name, @portfolio.avatar_urls.first)
-  #   binding.pry
-
-  #   puts 'hello'
-  # end
 end
 
 
-# class TestPortfolio
-#   attr_reader :full_name, :avatar_urls
-#   def initialize(full_name, avatar_urls)
-#     @full_name = full_name
-#     @avatar_urls = avatar_urls
-#   end
+class PortfolioForReact
+  attr_reader :portfolio
+  def initialize(full_name, avatar_urls, bio, locations, github_url, linkedin_url)
+    @portfolio = {
+      full_name => full_name,
+      avatar_urls => avatar_urls,
+      bio => bio,
+      locations => locations,
+      github_url => github_url,
+      linkedin_url => linkedin_url
+    }
+  end
 
-# end
+end
+
+
+# <%= react_component('Main', {portfolios: @portfolio.all,
+#   user: current_user,
+#   avatars: @portfolio.avatar_urls,
+#   bios: @portfolio.bios,
+#   locations: @portfolio.locations,
+#   port_asc: @portfolio.asc,
+#   port_desc: @portfolio.desc},
+# {prerender: true}) %>
+
+
+# <UserCard portfolios={newPortfolios}
+#  avatars={this.props.avatars}
+#  bios={this.props.bios}
+#  locations={this.props.locations}/>
