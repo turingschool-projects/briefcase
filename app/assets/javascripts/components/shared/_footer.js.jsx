3870ccea846990b83500b948142649210b381c23
Molly Brown <mollbrown@gmail.com>
2017-04-05 17:06:18 -0600

    add aria-label to font awesome logos on footer

--- app/assets/javascripts/components/shared/_footer.js.jsx
+++ app/assets/javascripts/components/shared/_footer.js.jsx
@@ -1,7 +1,7 @@
 var Footer = React.createClass({
   render: function() {
     return (
-        <footer className="page-footer">
+      <footer className="page-footer">
         <div className="row">
           <div className="col s4">
             <center><h2 className='newsletter'>Sign Up for our news letter</h2></center>
@@ -9,37 +9,43 @@ var Footer = React.createClass({
             <div className='input-for-news'>
           <center>
             <input className='input-newsletter' placeholder="Your Email Here">
-
-
             </input>
           </center>
             </div>
           </div>
-        <div className="col s4">
-          <center><h2 className='newsletter'>Social Links</h2></center>
-          <div>
-            <center>
+        </div>
 
-          <a href="https://github.com/turingschool"><i className="fa fa-github fa-3x social-footer" aria-hidden="true"></i></a>
-          <a href="https://twitter.com/turingschool"><i className="fa fa-twitter fa-3x social-footer" aria-hidden="true"></i></a>
-          <a href="https://www.facebook.com/turingschool"><i className="fa fa-facebook-official fa-3x social-footer" aria-hidden="true"></i></a>
-          <a href="https://www.instagram.com/turing_school/"><i className="fa fa-instagram fa-3x social-footer" aria-hidden="true"></i></a>
-          <a href="https://plus.google.com/110171830647821535352"><i className="fa fa-google fa-3x social-footer" aria-hidden="true"></i></a>
-          </center>
+          <div className="col s4">
+            <center><h2 className='newsletter'>Social Links</h2></center>
+            <div>
+              <center>
+                <a href="https://github.com/turingschool" aria-label="Turing Github"><i className="fa fa-github fa-3x social-footer" aria-hidden="true"></i></a>
+                <a href="https://twitter.com/turingschool" aria-label="Turing Twitter"><i className="fa fa-twitter fa-3x social-footer" aria-hidden="true"></i></a>
+                <a href="https://www.facebook.com/turingschool" aria-label="Turing Facebook"><i className="fa fa-facebook-official fa-3x social-footer" aria-hidden="true"></i></a>
+                <a href="https://www.instagram.com/turing_school/" aria-label="Turing Instagram"><i className="fa fa-instagram fa-3x social-footer" aria-hidden="true"></i></a>
+                <a href="https://plus.google.com/110171830647821535352" aria-label="Turing Google Plus"><i className="fa fa-google fa-3x social-footer" aria-hidden="true"></i></a>
+              </center>
+            </div>
           </div>
-      </div>
-      <div className="col s4">
-      <center><h2 className='newsletter'>Contact us</h2></center>
-      <center><p className='address'>Turing School of Software & Design
-          1331 17th Street, Suite LL100 -  Denver, CO 80202</p></center>
-        <center><p className='phone-number'>
-        ph: (303) 731-3117 fax: (303) 374-7360
-        contact@turing.io
-      </p></center>
-      </div>
-      </div>
 
-    </footer>
+          <div className="col s4">
+            <center>
+              <h2 className='newsletter'>Contact us</h2>
+            </center>
+            <center>
+              <p className='address'>Turing School of Software & Design
+                1331 17th Street, Suite LL100 -  Denver, CO 80202
+              </p>
+            </center>
+            <center>
+              <p className='phone-number'>
+              ph: (303) 731-3117 fax: (303) 374-7360
+              contact@turing.io
+              </p>
+            </center>
+          </div>
+
+      </footer>
     );
   }
 });
