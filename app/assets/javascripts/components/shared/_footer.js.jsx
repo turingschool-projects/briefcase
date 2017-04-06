2905bac0b344cfe61ae3fc6f5a22218dc77bed6e
Molly Brown <mollbrown@gmail.com>
2017-04-05 21:31:21 -0600

    fix footer background color and font sizing issues

--- app/assets/javascripts/components/shared/_footer.js.jsx
+++ app/assets/javascripts/components/shared/_footer.js.jsx
@@ -4,7 +4,7 @@ var Footer = React.createClass({
       <footer className="page-footer">
         <div className="row">
 
-          <div className="col s12 l4">
+          <div className="col s12 l4 newsletter">
             <center><h2 className='newsletter'>Sign Up for our news letter</h2></center>
             <center><p className='address'>Get the latest and greatest news and updates from the team here at Turing that you'll enjoy!</p></center>
             <div className='input-for-news'>
@@ -15,8 +15,8 @@ var Footer = React.createClass({
             </div>
           </div>
 
-          <div className="col s12 l4">
-            <center><h2 className='newsletter'>Social Links</h2></center>
+          <div className="col s12 l4 social-links">
+            <center><h2 className='social-links'>Social Links</h2></center>
             <div>
               <center>
                 <a href="https://github.com/turingschool" aria-label="Turing Github"><i className="fa fa-github fa-3x social-footer" aria-hidden="true"></i></a>
@@ -28,9 +28,9 @@ var Footer = React.createClass({
             </div>
           </div>
 
-          <div className="col s12 l4">
+          <div className="col s12 l4 contact">
             <center>
-              <h2 className='newsletter'>Contact us</h2>
+              <h2 className='contact'>Contact us</h2>
             </center>
             <center>
               <p className='address'>Turing School of Software & Design
