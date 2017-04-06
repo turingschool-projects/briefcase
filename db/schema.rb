c660c07cd97573b71859a7e6014b2d0cc89bc52c
Jason Conrad <jason.d.conrad.89@gmail.com>
2017-04-06 09:19:48 -0600

    add previous experience as text to the portfolio

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170405195519) do
+ActiveRecord::Schema.define(version: 20170406145124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 20170405195519) do
     t.string   "resume_content_type"
     t.integer  "resume_file_size"
     t.datetime "resume_updated_at"
+    t.text     "previous_experience"
     t.index ["user_id"], name: "index_portfolios_on_user_id", using: :btree
   end
 
