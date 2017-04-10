55894b85f67f208e3a78c4788575c9bee5d5357e
Molly Brown <mollbrown@gmail.com>
2017-04-10 15:10:21 -0600

    fix list nesting on alumni show page per accessability concerns

--- app/assets/javascripts/components/alumni/show/_alumni_jumbo_info.js.jsx
+++ app/assets/javascripts/components/alumni/show/_alumni_jumbo_info.js.jsx
@@ -23,18 +23,18 @@ var AlumniJumboInfo = React.createClass({
                   <div className="col s4 offset-s4">
                       <p>{portfolio.title}</p>
                   </div>
-                  <ul className='pull-right' >
+                  <ul className='pull-right'>
                     {this.props.resume != '/resumes/original/missing.png' &&
-                      <a href={this.props.resume} target="_blank"><li><button className='title btn transparent resume-btn'>Resume</button></li></a>
+                      <li><a href={this.props.resume} target="_blank"><li><button className='title btn transparent resume-btn'>Resume</button></li></a></li>
                     }
                     {portfolio.linkedin_url &&
-                      <a href={'http://' + this.stripUrl(portfolio.linkedin_url)}><li><button className='title btn transparent linkedin-btn'><i className="fa fa-linkedin fa-3x" aria-hidden="true" aria-label="linkedin"></i></button></li></a>
+                      <li><a href={'http://' + this.stripUrl(portfolio.linkedin_url)} aria-label="linkedIn URL"><li><button className='title btn transparent linkedin-btn' aria-label="linkedIn URL"><i className="fa fa-linkedin fa-3x" aria-hidden="true" aria-label="linkedin"></i></button></li></a></li>
                     }
                     {portfolio.twitter_url &&
-                      <a href={'http://' + this.stripUrl(portfolio.twitter_url)}><li><button className='title btn transparent linkedin-btn'><i className="fa fa-twitter fa-3x" aria-hidden="true" aria-label="twitter"></i></button></li></a>
+                      <li><a href={'http://' + this.stripUrl(portfolio.twitter_url)} aria-label="Twitter URL"><li><button className='title btn transparent linkedin-btn' aria-label="Twitter URL"><i className="fa fa-twitter fa-3x" aria-hidden="true" aria-label="twitter"></i></button></li></a></li>
                     }
                     {portfolio.personal_url &&
-                      <a href={'http://' + this.stripUrl(portfolio.personal_url)}><li><button className='title btn transparent linkedin-btn'><i className="fa fa-user fa-3x" aria-hidden="true" aria-label="personal site"></i></button></li></a>
+                      <li><a href={'http://' + this.stripUrl(portfolio.personal_url)} aria-label="Personal URL"><li><button className='title btn transparent linkedin-btn' aria-label="Personal Website URL"><i className="fa fa-user fa-3x" aria-hidden="true" aria-label="personal site"></i></button></li></a></li>
                     }
                   </ul>
                 </div>
