b5479785a45705696f47ddd4d45cc8251f62b3e1
Jason Conrad <jason.d.conrad.89@gmail.com>
2017-04-13 12:53:24 -0600

    Update seed:test_user to create users and portfolios with necessary information for search functionality

--- db/test_user.rb
+++ db/test_user.rb
@@ -1,25 +1,59 @@
-  def clean_users
-    User.delete_all
-  end
+require 'csv'
 
-  def create_users_and_portfolios
-    puts 'Start adding test users and test portfolios...'
-    puts 'Empty Users table'
-    clean_users
-    puts 'Adding test users first...'
-    user1 = User.create(first_name: 'Leslie', last_name: 'Hawk', email: 'example@example.com')
-    user2 = User.create(first_name: 'Joe', last_name: 'Henry', email: 'example@example.com')
-    user3 = User.create(first_name: 'Jack', last_name: 'Donley', email: 'example@example.com')
-    user4 = User.create(first_name: 'Greg', last_name: 'Maker', email: 'example@example.com')
-    user5 = User.create(first_name: 'Sue', last_name: 'Anne', email: 'example@example.com')
-    user6 = User.create(first_name: 'Sally', last_name: 'May', email: 'example@example.com')
+def start
+  puts 'Start seeding database...'
+  create_user
+  create_portfolio
+  puts 'Finished seeding database...'
+end
 
-    puts 'Adding test portfolios...'
-    user1.create_portfolio(full_name: 'Leslie Hawk', email: 'example@example.com', github_url: 'github.com', linkedin_url: 'linkedin.com', bio: 'I like cars', title: 'Software Developer')
-    user2.create_portfolio(full_name: 'Joe Henry', email: 'example@example.com', github_url: 'github.com', linkedin_url: 'linkedin.com', bio: 'I like cars', title: 'Software Developer')
-    user3.create_portfolio(full_name: 'Jack Donley', email: 'example@example.com', github_url: 'github.com', linkedin_url: 'linkedin.com', bio: 'I like cars', title: 'Software Developer')
-    user4.create_portfolio(full_name: 'Greg Maker', email: 'example@example.com', github_url: 'github.com', linkedin_url: 'linkedin.com', bio: 'I like cars', title: 'Software Developer')
-    user5.create_portfolio(full_name: 'Sue Anne', email: 'example@example.com', github_url: 'github.com', linkedin_url: 'linkedin.com', bio: 'I like cars', title: 'Software Developer')
-    user6.create_portfolio(full_name: 'Sally May', email: 'example@example.com', github_url: 'github.com', linkedin_url: 'linkedin.com', bio: 'I like cars', title: 'Software Developer')
-    puts 'Finished adding test users and test portfolios...'
+
+def cohort
+  ["1406", "1407", "1408", "1409", "1410", "1412", "1502", "1503", "1505", "1507", "1508", "1510", "1511", "1602", "1603", "1605", "1606", "1608", "1610", "1611"].sample
+end
+
+def create_previous_experience
+  ["Accounting", "Book Keeping", "Payroll Services", "Law", "Medical", "Customer Service", "Business", "Marketing", "Communications", "Management" ].sample
+end
+
+def create_user
+  200.times do
+    user = User.create!(
+    first_name:       Faker::Name.first_name,
+    last_name:        Faker::Name.last_name,
+    email:            Faker::Internet.free_email,
+    cohort:           cohort
+    )
   end
+end
+
+def add_locations
+  locations = Location.all
+  location = locations.sample
+end
+
+def clean_users
+  puts 'Emptying User table...'
+  User.delete_all
+end
+
+
+def create_portfolio
+  clean_users
+  users = User.all
+  users.each do |user|
+    user.portfolio = Portfolio.new(
+    email:                  user.email,
+    cohort:                 user.cohort,
+    github_url:             "#{user.first_name}.#{user.last_name}#{rand(0..100)}",
+    bio:                    Faker::Hipster.paragraph,
+    linkedin_url:           "#{user.first_name}.#{user.last_name}",
+    full_name:              "#{user.first_name} #{user.last_name}",
+    previous_experience:    create_previous_experience,
+    published:              true,
+    title:                  "Software Developer",
+    )
+    user.portfolio.locations << add_locations
+    user.portfolio.save
+  end
+end
